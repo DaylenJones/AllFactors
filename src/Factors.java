1202a717a62cfd81168570a57ec2977f18a41269
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-24 12:40:45 -0400

    update

--- src/Factors.java
+++ src/Factors.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Factors {
+public class Factors {
+
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+
+    private static void printFactors(int number) {
+
+
+        if (number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+
+
+
+        for (int i = 1; i <= number; i++) {
+
+
+            if (number % i == 0) {
+
+
+                System.out.println(i);
+            }
+        }
+    }
 }
